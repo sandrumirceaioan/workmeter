08ecfd56679cf2807d947185e2a3cde22ba9a43c
Sandru Mircea Ioan <sandrumirceaioan@gmail.com>
2018-03-07 16:33:15 +0200

    added MongoDB Atlas

--- server/app.module.ts
+++ server/app.module.ts
@@ -11,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ProjectsModule,
     ListsModule,
     MongooseModule.forRoot('mongodb://admin:rappac33!@ds247357.mlab.com:47357/tmwm')
+    // MongooseModule.forRoot('mongodb://sandrumirceaioan:rappac33!@cluster0-shard-00-00-xpraq.mongodb.net:27017,cluster0-shard-00-01-xpraq.mongodb.net:27017,cluster0-shard-00-02-xpraq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
   ],
   controllers: [AppController]
 })
